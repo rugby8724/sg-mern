e029dc8f11a6fb90f50e2b28128bd4144ecdb56a
Jeremiah <jeremiah.d.wise@gmail.com>
2023-03-18 10:40:10 -0500

    get controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controller/goalController')
 
-router.get('/', (req, res)=> {
-  res.status(200).json({message: 'Get Goals'})
-})
 
-router.post('/', (req, res)=> {
-  res.status(200).json({message: 'Create Goal'})
-})
 
-router.put('/:id', (req, res)=> {
-  res.status(200).json({message: `Update Goal ${req.params.id}`})
-})
+router.route('/').get(getGoals).post(setGoal)
 
-router.delete('/:id', (req, res)=> {
-  res.status(200).json({message: `Delete Goal ${req.params.id}`})
-})
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
